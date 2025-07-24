20a2e40efd56ea8b40e9a63fc2e8ff773d4ccd91
Tamim <tamimvaiya.123@gmail.com>
2025-07-24 11:05:11 +0600

    Added Detail View and Connected with Index View

--- ecom/urls.py
+++ ecom/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from shop.views import index
+from shop.views import index, detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('<int:id>/', detail, name='detail')
 ]
