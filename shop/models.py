c27317f8c7bd835e44d9cd65915d07ade79eb274
Tamim <tamimvaiya.123@gmail.com>
2025-07-24 08:18:21 +0600

    Added Products to Database

--- shop/models.py
+++ shop/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
+
+    def __str__(self):
+        return self.title
+
     title = models.CharField(max_length=200)
     price = models.FloatField()
     discount_price = models.FloatField()
@@ -9,4 +13,4 @@ class Product(models.Model):
     description = models.TextField()
     image = models.CharField(max_length=300)
 
-    
+
