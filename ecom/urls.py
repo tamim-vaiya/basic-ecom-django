fcee1b182cc0b996b45816b76bf8181a083c7128
Tamim <tamimvaiya.123@gmail.com>
2025-07-26 23:31:27 +0600

    Added Checkout Template

--- ecom/urls.py
+++ ecom/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from shop.views import index, detail
+from shop.views import index, detail, checkout
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
-    path('<int:id>/', detail, name='detail')
+    path('<int:id>/', detail, name='detail'),
+    path('checkout/', checkout, name='checkout')
 ]
