36c48069c5d54f21a126fe241d4120c04d26139c
Tamim <tamimvaiya.123@gmail.com>
2025-07-27 18:18:28 +0600

    Added Cart Items To Database

--- shop/views.py
+++ shop/views.py
@@ -25,11 +25,12 @@ def detail(request, id):
 def checkout(request):
 
     if request.method == "POST":
+        items = request.POST.get('items',"")
         name = request.POST.get('name', "")
         email = request.POST.get('email', "")
         address = request.POST.get('address', "")
 
-        order = Order(name=name, email=email, address=address)
+        order = Order(items=items, name=name, email=email, address=address)
         order.save()
 
     return render(request, 'shop/checkout.html')
